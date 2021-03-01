fd5d652e6919a451554b8566fef0dc7f3b82d948
E_Ye <E_Ye@outlook.com>
2021-03-01 19:06:45 +0800

    fairygui 提交

--- Unity/Assets/Model/Module/FUI/FUILoading/LoadingUIController.cs
+++ Unity/Assets/Model/Module/FUI/FUILoading/LoadingUIController.cs
@@ -9,6 +9,7 @@ namespace ETModel
     {
         public override void Run()
         {
+            Log.Debug(">>>EventIdType.LoadingBegin");
             Game.Scene.GetComponent<FUIPackageComponent>().AddPackage(FUILoadingMain.UIPackageName);
             FUI fui = FUILoadingFactory.Create();
             Game.Scene.GetComponent<FUIComponent>().Add(fui);
@@ -21,6 +22,7 @@ namespace ETModel
     {
         public override void Run()
         {
+            Log.Debug(">>>EventIdType.LoadingFinish");
             Game.Scene.GetComponent<FUIComponent>().Get(FUILoadingMain.UIResName).Visible = false;
             Log.Info("加载UI关闭");
         }
