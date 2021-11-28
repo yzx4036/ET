43bfa4212ea931d45533915c9bd3be4804039bb7
E_Ye <E_Ye@outlook.com>
2021-11-28 16:14:24 +0800

    导入FairyGUI 有报错

--- Unity/Codes/Model/Core/Object/SEyesSoft/EntityEx.cs
+++ Unity/Codes/Model/Core/Object/SEyesSoft/EntityEx.cs
@@ -4,9 +4,19 @@ namespace ET
 {
     public partial class Entity
     {
-        // public static Entity CreateE(Type type, bool isFromPool)
-        // {
-            // return Create(type, isFromPool);
-        // }
+        public static Entity CreateE(Type type, bool isFromPool)
+        {
+            return Create(type, isFromPool);
+        }
+        
+        public Entity AddChildByType(Type type, bool isFromPool = false) where T : Entity
+        {
+            Entity component = Entity.Create(type, isFromPool);
+            component.Id = IdGenerater.Instance.GenerateId();
+            component.Parent = this;
+
+            EventSystem.Instance.Awake(component);
+            return component;
+        }
     }
 }
---
+++ Unity/Codes/Model/ThirdParty/FairyGUI/Core/FUIInitComponent.cs
@@ -0,0 +1,51 @@
+﻿
+using System;
+using System.Collections.Generic;
+using FairyGUI;
+using static FairyGUI.UIContentScaler;
+
+namespace ET
+{
+    public class FUIInitComponent: Entity
+    {
+        private readonly string[] _initPackageArray =
+        {
+            FUIPackage.CommonAsset,
+            FUIPackage.CommonComp,
+            // FUIPackage.FLogin,
+        };
+        public async ETTask Init()
+        {
+            GRoot.inst.SetContentScaleFactor(1080,1920,ScreenMatchMode.MatchWidthOrHeight);
+            UIConfig.defaultFont = "KaiTi";
+
+            for (int i = 0; i < _initPackageArray.Length; i++)
+            {
+                await Game.Scene.GetComponent<FUIPackageComponent>().AddPackageAsync(_initPackageArray[i]);
+            }
+            
+            // await Game.Scene.GetComponent<FUIPackageComponent>().AddPackageAsync(FUIPackage.FLogin);
+            // await Game.Scene.GetComponent<FUIPackageComponent>().AddPackageAsync(FUIPackage.UICharacterSelect);
+            // await Game.Scene.GetComponent<FUIPackageComponent>().AddPackageAsync(FUIPackage.UIMain);
+            // await Game.Scene.GetComponent<FUIPackageComponent>().AddPackageAsync(FUIPackage.UIEquip);
+            // await Game.Scene.GetComponent<FUIPackageComponent>().AddPackageAsync(FUIPackage.UIBag);
+            // await Game.Scene.GetComponent<FUIPackageComponent>().AddPackageAsync(FUIPackage.UIBattle);
+            // await Game.Scene.GetComponent<FUIPackageComponent>().AddPackageAsync(FUIPackage.UISetting);
+            // await Game.Scene.GetComponent<FUIPackageComponent>().AddPackageAsync(FUIPackage.UITips);
+            // await Game.Scene.GetComponent<FUIPackageComponent>().AddPackageAsync(FUIPackage.UIShop);
+
+
+        }
+
+        public override void Dispose()
+        {
+            if (IsDisposed)
+            {
+                return;
+            }
+
+            Game.Scene.GetComponent<FUIPackageComponent>().RemovePackage(FUIPackage.FLogin);
+            base.Dispose();
+        }
+    }
+}
