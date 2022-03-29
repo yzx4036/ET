bff80001bb5ff6df6f591065b4b9c0433bc5a6ae
E_Ye <E_Ye@outlook.com>
2022-03-29 08:23:01 +0800

    修复ileuntime 加载配置报错

--- Unity/Codes/Hotfix/Module/Config/ConfigComponentSystem.cs
+++ Unity/Codes/Hotfix/Module/Config/ConfigComponentSystem.cs
@@ -4,79 +4,103 @@ using System.Threading.Tasks;
 
 namespace ET
 {
-	[ObjectSystem]
-    public class ConfigAwakeSystem : AwakeSystem<ConfigComponent>
+    [ObjectSystem]
+    public class ConfigAwakeSystem: AwakeSystem<ConfigComponent>
     {
         public override void Awake(ConfigComponent self)
         {
-	        ConfigComponent.Instance = self;
+            ConfigComponent.Instance = self;
         }
     }
-    
+
     [ObjectSystem]
-    public class ConfigDestroySystem : DestroySystem<ConfigComponent>
+    public class ConfigDestroySystem: DestroySystem<ConfigComponent>
     {
-	    public override void Destroy(ConfigComponent self)
-	    {
-		    ConfigComponent.Instance = null;
-	    }
+        public override void Destroy(ConfigComponent self)
+        {
+            ConfigComponent.Instance = null;
+        }
     }
-    
+
     public static class ConfigComponentSystem
-	{
-		public static async Task LoadOneConfig(this ConfigComponent self, Type configType)
-		{
-			byte[] oneConfigBytes = await self.ConfigLoader.GetOneConfigBytes(configType.FullName);
+    {
+        public static async Task LoadOneConfig(this ConfigComponent self, Type configType)
+        {
+            byte[] oneConfigBytes = await self.ConfigLoader.GetOneConfigBytes(configType.FullName);
 
-			object category = ProtobufHelper.FromBytes(configType, oneConfigBytes, 0, oneConfigBytes.Length);
+            object category = ProtobufHelper.FromBytes(configType, oneConfigBytes, 0, oneConfigBytes.Length);
 
-			self.AllConfig[configType] = category;
-		}
-		
-		public static void Load(this ConfigComponent self)
-		{
-			self.AllConfig.Clear();
-			HashSet<Type> types = Game.EventSystem.GetTypes(typeof (ConfigAttribute));
-			
-			Dictionary<string, byte[]> configBytes = new Dictionary<string, byte[]>();
-			self.ConfigLoader.GetAllConfigBytes(configBytes);
+            self.AllConfig[configType] = category;
+        }
 
-			foreach (Type type in types)
-			{
-				self.LoadOneInThread(type, configBytes);
-			}
-		}
-		
-		public static async ETTask LoadAsync(this ConfigComponent self)
-		{
-			self.AllConfig.Clear();
-			HashSet<Type> types = Game.EventSystem.GetTypes(typeof (ConfigAttribute));
-			
-			Dictionary<string, byte[]> configBytes = new Dictionary<string, byte[]>();
-			await self.ConfigLoader.GetAllConfigBytes(configBytes);
+        public static void Load(this ConfigComponent self)
+        {
+            self.AllConfig.Clear();
+            HashSet<Type> types = Game.EventSystem.GetTypes(typeof (ConfigAttribute));
 
-			using (ListComponent<Task> listTasks = ListComponent<Task>.Create())
-			{
-				foreach (Type type in types)
-				{
-					Task task = Task.Run(() => self.LoadOneInThread(type, configBytes));
-					listTasks.Add(task);
-				}
+            Dictionary<string, byte[]> configBytes = new Dictionary<string, byte[]>();
+            self.ConfigLoader.GetAllConfigBytes(configBytes);
 
-				await Task.WhenAll(listTasks.ToArray());
-			}
-		}
+            foreach (Type type in types)
+            {
+                if (configBytes.ContainsKey(type.Name))
+                {
+                    self.LoadOneInThread(type, configBytes[type.Name]);
+                }
+            }
+        }
 
-		private static void LoadOneInThread(this ConfigComponent self, Type configType, Dictionary<string, byte[]> configBytes)
-		{
-			byte[] oneConfigBytes = configBytes[configType.Name];
+        public static async ETTask LoadAsync(this ConfigComponent self)
+        {
+            self.AllConfig.Clear();
+            HashSet<Type> types = Game.EventSystem.GetTypes(typeof (ConfigAttribute));
 
-			object category = ProtobufHelper.FromBytes(configType, oneConfigBytes, 0, oneConfigBytes.Length);
+            Dictionary<string, byte[]> configBytes = new Dictionary<string, byte[]>();
+            await self.ConfigLoader.GetAllConfigBytes(configBytes);
+            foreach (var configByte in configBytes)
+            {
+                Log.Debug($">>>>>>>>>>>configByte {configByte.Key}");
+            }
 
-			lock (self)
-			{
-				self.AllConfig[configType] = category;	
-			}
-		}
-	}
+            using (ListComponent<Task> listTasks = ListComponent<Task>.Create())
+            {
+                foreach (Type type in types)
+                {
+                    if (configBytes.ContainsKey(type.Name))
+                    {
+                        Task task = Task.Run(() =>
+                        {
+                            self.LoadOneInThread(type, configBytes[type.Name]);
+                        });
+                        listTasks.Add(task);
+                    }
+                }
+
+                await Task.WhenAll(listTasks.ToArray());
+            }
+        }
+
+        private static void LoadOneInThread(this ConfigComponent self, Type configType, byte[] oneConfigBytes)
+        {
+            try
+            {
+                if (oneConfigBytes == null)
+                {
+                    return;
+                }
+                Log.Debug($">>>>>>>>>>>LoadOneInThread   configType:{configType.Name} {oneConfigBytes.Length}");
+                object category = ProtobufHelper.FromBytes(configType, oneConfigBytes, 0, oneConfigBytes.Length);
+                Log.Debug($">>>>>>>>>>>LoadOneInThread1111111111   configType:{configType.Name} {oneConfigBytes.Length}");
+
+                lock (self)
+                {
+                    self.AllConfig[configType] = category;
+                }
+            }
+            catch (Exception e)
+            {
+                Log.Error($"type:{configType.Name}!!!!!!!!!!!!!!!!!!!!!!><<<{e}");
+            }
+        }
+    }
 }
--- Unity/Codes/HotfixView/AppStart_Init.cs
+++ Unity/Codes/HotfixView/AppStart_Init.cs
@@ -14,7 +14,7 @@ namespace ET
             Game.Scene.AddComponent<AddressablesResComponent>();
             // await ResourcesComponent.Instance.LoadBundleAsync("config.unity3d");
             Game.Scene.AddComponent<ConfigComponent>();
-            await ConfigComponent.Instance.LoadAsync();
+            ConfigComponent.Instance.Load();
             // ResourcesComponent.Instance.UnloadBundle("config.unity3d");
             
             Game.Scene.AddComponent<OpcodeTypeComponent>();
