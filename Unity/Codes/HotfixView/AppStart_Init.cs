a0e6e36222eb00cecc50d1b5376369e5ef98753e
E_Ye <E_Ye@outlook.com>
2022-08-31 22:59:56 +0800

    $Client-#- 删除一些注释

--- Unity/Codes/HotfixView/AppStart_Init.cs
+++ Unity/Codes/HotfixView/AppStart_Init.cs
@@ -16,12 +16,9 @@ namespace ET
             Game.Scene.AddComponent<TimerComponent>();
             Game.Scene.AddComponent<CoroutineLockComponent>();
             // 加载配置
-            // Game.Scene.AddComponent<ResourcesComponent>();
             Game.Scene.AddComponent<AddressablesResComponent>();
-            // await ResourcesComponent.Instance.LoadBundleAsync("config.unity3d");
             Game.Scene.AddComponent<ConfigComponent>();
             await ConfigComponent.Instance.LoadAsync();
-            // ResourcesComponent.Instance.UnloadBundle("config.unity3d");
             
             Game.Scene.AddComponent<OpcodeTypeComponent>();
             Game.Scene.AddComponent<MessageDispatcherComponent>();
@@ -33,7 +30,7 @@ namespace ET
             Game.Scene.AddComponent<GlobalComponent>();
             Game.Scene.AddComponent<NumericWatcherComponent>();
             Game.Scene.AddComponent<AIDispatcherComponent>();
-            // await ResourcesComponent.Instance.LoadBundleAsync("unit.unity3d");
+            
             var _fUIInitComponent = Game.Scene.AddComponent<FUIInitComponent>();
             await _fUIInitComponent.Init();
             Scene zoneScene = SceneFactory.CreateZoneScene(1, "Game", Game.Scene);
