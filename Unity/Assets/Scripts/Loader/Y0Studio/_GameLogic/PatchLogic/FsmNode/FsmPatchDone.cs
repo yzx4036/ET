69832dcdcc6484baaae41b917db0477f84c383ee
E_Ye <E_Ye@outlook.com>
2023-02-25 01:43:29 +0800

    YooAsset升级流程

---
+++ Unity/Assets/Scripts/Loader/Y0Studio/_GameLogic/PatchLogic/FsmNode/FsmPatchDone.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniFramework.Machine;
+using UniFramework.Module;
+
+namespace Y0Studio.ET.Client
+{
+	/// <summary>
+	/// 流程更新完毕
+	/// </summary>
+	internal class FsmPatchDone: IStateNode
+	{
+		void IStateNode.OnCreate(StateMachine machine)
+		{
+		}
+
+		void IStateNode.OnEnter()
+		{
+			PatchEventDefine.PatchStatesChange.SendEventMessage("开始游戏！");
+
+
+		}
+
+		void IStateNode.OnUpdate()
+		{
+		}
+
+		void IStateNode.OnExit()
+		{
+		}
+	}
+}
