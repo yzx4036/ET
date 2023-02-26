1ef2e036efa194d1bf6085ad0083a027f2b98e5b
E_Ye <E_Ye@outlook.com>
2023-02-26 23:03:36 +0800

    $Client-#- 接入YooAsset更新流程，显示更新流程UI

--- Unity/Assets/Scripts/Loader/Y0Studio/_GameLogic/PatchLogic/FsmNode/FsmPatchPrepare.cs
+++ Unity/Assets/Scripts/Loader/Y0Studio/_GameLogic/PatchLogic/FsmNode/FsmPatchPrepare.cs
@@ -23,8 +23,8 @@ namespace Y0Studio.ET.Client
 		void IStateNode.OnEnter()
 		{
 			// 加载更新面板
-			// var go = Resources.Load<GameObject>("PatchWindow");
-			// GameObject.Instantiate(go);
+			var go = Resources.Load<GameObject>("PatchWindow");
+			GameObject.Instantiate(go);
 
 			_machine.ChangeState<FsmInitialize>();
 		}
